872d36c1914781480b41962247be822124fdeea6
aksel26 <kevinxkim2023@gmail.com>
2025-11-24 17:12:12 +0900

    홈 페이지: 외부 페이지에서의 섹션 스크롤 이동 기능 구현

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Hero from '../sections/Hero'
 import About from '../sections/About'
 import Projects from '../sections/Projects'
@@ -5,8 +7,24 @@ import Experience from '../sections/Experience'
 import Contact from '../sections/Contact'
 
 export default function Home() {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.hash) {
+      const targetId = location.hash.replace('#', '')
+      const element = document.getElementById(targetId)
+      const mainElement = document.querySelector('main')
+
+      if (element && mainElement) {
+        setTimeout(() => {
+          element.scrollIntoView({ behavior: 'smooth' })
+        }, 100)
+      }
+    }
+  }, [location])
+
   return (
-    <main>
+    <main className="h-screen overflow-y-scroll snap-y snap-mandatory scroll-smooth">
       <Hero />
       <About />
       <Projects />
